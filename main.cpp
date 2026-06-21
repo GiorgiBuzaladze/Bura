035d5688dff123b9c667a45746cf361b535faad9
“Buzala“ <giorgibuzaladze2608@gmail.com>
2026-06-21 20:52:40 +0400

    added management system, the main game driver needs to be thought about

--- main.cpp
+++ main.cpp
@@ -66,9 +66,7 @@ struct Card{
 	}
 };
 typedef std::vector<Card> Deck;
-void generateDeck(Deck &initialDeck, int &dealt);
-void dealCards(Deck &orderedDeck, Deck &playerHand, int &dealt, int dealCount = 5);
-Card generateTrumpCard(Deck &initialDeck, int &dealt);
+void generateDeck(Deck &initialDeck); 
 
 class Player{
 	private:
@@ -76,19 +74,23 @@ class Player{
 		int currentScore;
 		int generalScore;
 		int d;
-		Deck oDeck;
 	public:
-	Player(Deck &orderedDeck, int &dealt) : oDeck(orderedDeck), d(dealt){
-		dealCards(oDeck, currentDeck, d, 5);
+	Player(){
 		currentScore = 0;
 		generalScore = 0;
-		std::cout << "Dealt Deck: " << std::endl;
-		for(const auto& card : currentDeck){
-			std::cout << card;
-		}
+	}
+	Deck& getCurrentDeck(){
+		return currentDeck;
 	}
 	bool selected = false;
 	Deck selectedCards;
+	int &getCurrentScore(){
+		return currentScore;
+	}
+	int &getGeneralScore(){
+		return generalScore;
+	}
+	
 	void selectCards(){
 		int n;
 		std::cout << "\nHow many cards to select: ";
@@ -117,88 +119,101 @@ class Player{
 				std::cout << card;
 				currentDeck.erase(std::find(currentDeck.begin(), currentDeck.end(), card));
 			}
+			selectedCards.clear();
 		}
 		else{
 			std::cout << "No cards have been selected!" << std::endl;
 		}
 	}	
+};
+class GameManagementSystem{
+	private:
+		Player playerOne;
+		Player playerTwo;
+		Deck orderedDeck;
+		int dealtAmount = 0;
+		std::pair<int, int> generalScore;
+	public:
+	int starting; //if == 0, player one starts, if == 2 player two starts
+	GameManagementSystem(Deck oDeck) : orderedDeck(oDeck){
+		std::mt19937 static gen(std::random_device{}());
+		std::uniform_int_distribution<> range(0,1);
+		starting = range(gen);
+		
+		dealCards(orderedDeck, playerOne.getCurrentDeck(), dealtAmount, 5);
+		dealCards(orderedDeck, playerTwo.getCurrentDeck(), dealtAmount, 5);
+		
+		std::cout << "Trump Card: " << generateTrumpCard(orderedDeck, dealtAmount);
+		
+		generalScore.first = playerOne.getGeneralScore();
+		generalScore.second = playerTwo.getGeneralScore();
+	}
+	void dealCards(Deck &orderedDeck, Deck &playerHand, int &dealt, int dealCount){
+		try{
+			if(dealt == 36){
+				throw std::invalid_argument("All the cards have been dealt!");
+				return;
+			}
+			if(dealt + dealCount > 36){
+				dealCount = 36 - dealt;
+			}	
+			for(int i = 0; i < dealCount; i++){
+				playerHand.push_back(orderedDeck[dealt]);
+				dealt++;
+			}
+		}
+			catch(std::invalid_argument &error){
+				throw error;
+			}	
+		}
+	Card generateTrumpCard(Deck &orderedDeck, int &dealt){
+		Card trump = orderedDeck[0];
+		dealt++;
+		return trump;
+	}	
 	void addCards(){
 		try{
-			dealCards(oDeck, currentDeck, d, (5 - currentDeck.size()));
-			std::cout << "\nCards Added! Current Deck: " << std::endl;
-			for(const auto& card : currentDeck){
+			dealCards(orderedDeck, playerOne.getCurrentDeck(), dealtAmount, (5 - playerOne.getCurrentDeck().size()));
+			dealCards(orderedDeck, playerTwo.getCurrentDeck(), dealtAmount, (5 - playerOne.getCurrentDeck().size()));
+			std::cout << "\nCards Added! Player One Current Deck: " << std::endl;
+			for(const auto& card : playerOne.getCurrentDeck()){
+				std::cout << card << " ";
+			}
+			std::cout << "\n\nPlayer Two Current Deck: " << std::endl;
+			for(const auto& card : playerTwo.getCurrentDeck()){
 				std::cout << card << " ";
 			}
 		}
 		catch(std::invalid_argument &error){
 			std::cout << error.what() << std::endl;
 		}
+	}	
+	void mainGame(){
+		while(generalScore.first != 11 || generalScore.second != 11){
+			
+		}
 	}
-	
 };
-//class GameManagementSystem{
-//	private:
-//		Player playerOne;
-//		Player playerTwo;
-//}
 
 
 
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
-	
-	
-	
-	int dealt = 0;
-	
+
 	Deck initialDeck;
-	generateDeck(initialDeck, dealt);
+	generateDeck(initialDeck);
 	
-	Card trumpCard = generateTrumpCard(initialDeck, dealt);
-	std::cout << "Trump Card: " << trumpCard << std::endl;;
-	
-	Player playerOne(initialDeck, dealt);
-	
-	while(1 < 5){
-		playerOne.selectCards();
-		playerOne.playCard();
-		playerOne.addCards();
-	}
+	GameManagementSystem gms(initialDeck);
+	gms.addCards();
 	
 	
 }
-void generateDeck(Deck &initialDeck, int &dealt){
-	
+void generateDeck(Deck &initialDeck){
 	for(int i = 1; i <= 4; i++){
 		for(int j = 1; j <= 9; j++){
 			initialDeck.push_back(Card(static_cast<Suit>(i), static_cast<Rank>(j)));
 		}
 	}
-	
 	std::shuffle(initialDeck.begin(), initialDeck.end(), std::mt19937{std::random_device{}()});
 	
-}
-void dealCards(Deck &orderedDeck, Deck &playerHand, int &dealt, int dealCount){
-	try{
-		if(dealt == 36){
-			throw std::invalid_argument("All the cards have been dealt!");
-			return;
-		}
-		if(dealt + dealCount > 36){
-			dealCount = 36 - dealt;
-		}	
-		for(int i = 0; i < dealCount; i++){
-			playerHand.push_back(orderedDeck[dealt]);
-			dealt++;
-		}
-	}
-	catch(std::invalid_argument &error){
-		throw error;
-	}
-	
-}
-Card generateTrumpCard(Deck &orderedDeck, int &dealt){
-	Card trump = orderedDeck[0];
-	dealt++;
-	return trump;
 }
